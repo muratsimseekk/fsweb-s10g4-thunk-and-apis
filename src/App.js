e046cfe9c3c14493912179ae51597a0bf11a7386
Murat Simsek <muratsimseekk@gmail.com>
2023-11-23 17:05:45 +0300

    Give me another button provides another image

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ export default function App() {
 
           <div className="flex gap-3 justify-end py-3">
             <button
+            onClick={() => dispatch(fetchAnother())}
               className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
             >
               Başka bir tane
